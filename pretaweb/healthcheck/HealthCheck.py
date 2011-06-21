92298eaf127dd7145ae1126b769c91f2d7f15428
Adam Terrey <arterrey@pretaweb.com>
2011-06-21 12:36:08 +1000

    Added subresouce pre-caching from parsed frontpage

--- pretaweb/healthcheck/HealthCheck.py
+++ pretaweb/healthcheck/HealthCheck.py
@@ -1,7 +1,10 @@
 
 import sys
 import traceback
+import urllib
+import re
 from StringIO import StringIO
+from lxml import etree
 from Products.Five import BrowserView
 from zope.component import getMultiAdapter
 from plone.subrequest import subrequest
@@ -10,10 +13,51 @@ from plone.subrequest import subrequest
 healthCheckDone = False
 
 
+cssImportPatern = re.compile (r"@import\s+url\(([^)]+)\)\s*;")
+
+
 class RequestError (Exception): pass
 
 
 class HealthCheck (BrowserView):
+
+
+
+
+    def parseLinks (self, workingPath, links):
+	base = self.request.base
+	workingURL = base + workingPath
+	output = self.output
+
+        newLinks = set()
+        for l in links:
+	    if len(l) > 0:
+	        l = l.split("#")[0]
+		l = urllib.unquote(l)
+
+		if l[0] == "/":
+		    l = base + l
+
+		elif workingURL == l:
+		    pass
+
+		elif l.startswith(base):
+		    p = l[len(base):]
+		    newLinks.add (p)
+
+
+		elif not (l.startswith("http://") or l.startswith("https://")):
+		    ll = workingURL + "/" + l
+		    p = ll[len(base):]
+		    newLinks.add(p)
+
+		else:
+		    output.write ("\tResource out of scope: %s\n" % l)
+
+
+        return newLinks
+	        
+
 
 
     def wakePlone (self, plone):
@@ -40,8 +84,51 @@ class HealthCheck (BrowserView):
 
         # Process output
 
+        byteCount = 0
         body = response.getBody()
-        output.write ("\tfront page size: %s\n" % len(body))
+	byteCount += len(body)
+
+	try:
+	    doc = etree.parse (StringIO(body), etree.HTMLParser())
+	except etree.XMLSyntaxError:
+	    output.write ("\tWarning: XMLSyntaxError on front page\n")
+	else:
+	    links = doc.xpath("/html/body//a/@href")
+            images = doc.xpath("/html/body//img/@src")
+	    headLink = doc.xpath ("/html//link/@href")
+	    scripts = doc.xpath ("/html//script/@src")
+
+            cssImports = []
+	    for mo in cssImportPatern.finditer(body):
+	        groups = mo.groups()
+		if len(groups) > 0:
+		    cssImports.append(groups[0])
+
+	    resources = self.parseLinks (url_path, links)
+            resources = resources.union( self.parseLinks (url_path, images) )
+            resources = resources.union( self.parseLinks (url_path, headLink) )
+            resources = resources.union( self.parseLinks (url_path, scripts) )
+            resources = resources.union( self.parseLinks (url_path, cssImports) )
+
+	    output.write ("\tFound %s sub resources to load.\n" % len(resources))
+
+	    for resURL in resources:
+	        resResponse = subrequest(resURL)
+		resStatus = resResponse.getStatus()
+		if resStatus == 200:
+		    resBody = resResponse.getBody()
+		    byteCount += len(resBody)
+
+		    # output.write ("\tGot status %s for resource: %s\n" % (resStatus, resURL))
+
+		else:
+		    output.write ("\tGot status %s for resource: %s\n" % (resStatus, resURL))
+	        
+
+        output.write ("\tNumber of bytes retrieved: %s\n" % len(body))
+
+
+
 
 
 
